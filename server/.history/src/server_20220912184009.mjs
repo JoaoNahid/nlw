2b26abf01df5bb0bfcf42cb4ad684a2a51dcdf98
Joao Nahid <nahid@webtagger.com.br>
2022-09-12 21:50:59 -0300

    inicio aula 2

--- server/.history/src/server_20220912184009.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get('/adds', () => {
-  console.log("Acessou adds");
-})
-
-app.listen(3333)
--- server/.history/src/server_20220912184928.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get('/ads', (request, response) => {
-  return response.send
-})
-
-app.listen(3333)
